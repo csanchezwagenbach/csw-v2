08781787ff19f03cf532aee2e3625464513e6fec
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-02-09 09:45:51 -0800

    rendering navbar template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { ThemeProvider } from 'react-bootstrap';
+import NavigationHeader from './components/Header';
 
 function App() {
   return (
-    <div>
-      <p>Got it</p>
-    </div>  
+    <ThemeProvider
+      breakpoints={['l', 'm', 's']}
+    >
+      <div>
+        <NavigationHeader />
+      </div>
+    </ThemeProvider>
   );
 }
 
