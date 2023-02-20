36719115b9bc1bf0e1b55f976f1d67505097c804
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-02-20 14:52:38 -0800

    completed projects array

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,7 +4,7 @@ export default function Projects () {
 
     const projects = [
         {
-            title: "chef-gpt",
+            title: "Chef-GPT",
             repository: "https://github.com/csanchezwagenbach/chef-gpt",
             url: "https://boiling-springs-79959.herokuapp.com/login",
             description: "A full stack application guiding a user's interaction with OpenAI's GPT-3 models through a questionnaire. Through mediated interactions, casual users may most effectively leverage artificial intelligence assets. This application serves as sample demonstration of mediated interactions maximizing an artificial intelligences use-value.",
@@ -23,6 +23,27 @@ export default function Projects () {
             url: "https://floating-springs-81705.herokuapp.com/",
             description: "Full stack CMS utilizing the Model-View-Controller paradigm to organize application functionality.",
             tags: "CMS, MySQL, Express, Node.js"
+        },
+        {
+            title: "Employee-Tracker CMS",
+            repository: "https://github.com/csanchezwagenbach/employee-tracker-cms",
+            url: "https://github.com/csanchezwagenbach/employee-tracker-cms",
+            description: "Command-line application utilizing Node.js, Inquirer, and MySQL to create an interactive sample of a content management system",
+            tags: "CLI, CMS, MySQL, Node.js"
+        },
+        {
+            title: "Sample Social Network API",
+            repository: "https://github.com/csanchezwagenbach/sample-social-network-api",
+            url: "https://github.com/csanchezwagenbach/sample-social-network-api",
+            description: "Sample of a functioning social network espress.js API configured with Mongoose to interact with a Mongo database.",
+            tags: "REST API, Express, MongoDB, Mongoose"
+        },
+        {
+            title: "Sample E-Commerce API",
+            repository: "https://github.com/csanchezwagenbach/e-commerce-back-end-sample",
+            url: "https://github.com/csanchezwagenbach/e-commerce-back-end-sample",
+            description: "Sample of a functioning e-commerce express.js API configured with Sequelize to interact with a MySQL database.",
+            tags: "REST API, Express, MySQL, Sequelize"
         }
     ]
     return (
