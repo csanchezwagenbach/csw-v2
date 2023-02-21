46cfa94bd3667fbab51526ac5594c038b11b6005
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-02-20 18:23:45 -0800

    built footer, added in links and icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import Footer from './components/Footer';
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import linkedin from '../assets/linkedlogo.png';
+import twitter from '../assets/twitlogo.png';
+import github from '../assets/gitlogo.png';
 
-export default function Footer () {
+export default function Footer() {
     return (
-        <div className='row justify-content-center'>
-            <a href="#">Herro</a>
+        <div className='row justify-content-center my-2'>
+            <a className='col-4 text-center' href="https://twitter.com/colbysanchbach"><img className='logo' src={twitter
+            }></img></a>
+            <a className='col-4 text-center' href="https://github.com/csanchezwagenbach"><img className='logo' src={github}></img></a>
+            <a className='col-4 text-center' href="https://www.linkedin.com/in/colby-wagenbach/"><img className='logo' src={linkedin}></img></a>
         </div>
     )
 }
